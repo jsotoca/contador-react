ba77e5d6a32afc70d8e937232734aecfcc00f53b
jsotoca <jsotoca@unprg.edu.pe>
2021-04-04 20:41:30 -0500

    Se configuro setvalor en contador

--- src/componentes/contador/Contador.js
+++ src/componentes/contador/Contador.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function Contador() {
     const [valor, setValor] = useState(0);
+
     return (
         <>
-            <h1>{valor}</h1>    
+            <h1>{valor}</h1>
+            <button onClick={ () => setValor(valor + 1)}>+1</button>
+            <button onClick={ () => setValor(valor - 1)}>-1</button>
+            <button onClick={ () => setValor(0)}>reset</button>    
         </>
     );
 }
